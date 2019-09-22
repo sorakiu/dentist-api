a9fa8783edffb55d623b5fd141a8fbd0a7e78240
David Moore <sorakiu@gmail.com>
2019-09-22 19:28:48 -0400

    clean up, make test data-driven

--- src/test/groovy/me/davidmoore/dentistapi/controllers/AppointmentsControllerSpec.groovy
+++ src/test/groovy/me/davidmoore/dentistapi/controllers/AppointmentsControllerSpec.groovy
@@ -7,9 +7,6 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import me.davidmoore.dentistapi.config.MockConfig
 import me.davidmoore.dentistapi.models.Appointment
 import me.davidmoore.dentistapi.repositories.AppointmentRepository
-import org.mockito.Mockito
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -18,7 +15,6 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import spock.lang.Specification
 
@@ -26,15 +22,12 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
-import org.mockito.Mockito
-
 @AutoConfigureMockMvc
 @WebMvcTest(AppointmentsController.class)
 @ContextConfiguration(classes = MockConfig.class)
 class AppointmentsControllerSpec extends Specification {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AppointmentsControllerSpec.class)
-
+    @SuppressWarnings("unused")
     @MockBean
     private AppointmentRepository appointmentRepository
 
@@ -49,27 +42,17 @@ class AppointmentsControllerSpec extends Specification {
         LocalDateTimeDeserializer dateTimeDeserializer = new LocalDateTimeDeserializer(formatter)
         LocalDateTimeSerializer dateTimeSerializer = new LocalDateTimeSerializer(formatter)
 
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addDeserializer(LocalDateTime.class, dateTimeDeserializer);
-        javaTimeModule.addSerializer(LocalDateTime.class, dateTimeSerializer);
+        JavaTimeModule javaTimeModule = new JavaTimeModule()
+        javaTimeModule.addDeserializer(LocalDateTime.class, dateTimeDeserializer)
+        javaTimeModule.addSerializer(LocalDateTime.class, dateTimeSerializer)
 
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(javaTimeModule);
+        objectMapper = new ObjectMapper()
+        objectMapper.registerModule(javaTimeModule)
     }
 
-    def setup() {
-    }
-
-
-    def "CreateAppointment succeeds only with valid values"() {
-        given: "a valid appointment"
-            def now = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES)
-            def startTime = now.plusHours(1)
-            def rawAppointment = new Appointment(startTime: startTime,
-                    endTime: startTime.plusMinutes(30),
-                    dentistId: 1,
-                    patientId: 1
-            )
+    def "CreateAppointment succeeds only with valid values"(LocalDateTime now, LocalDateTime startTime, LocalDateTime endTime, int expectedResponseCode) {
+        given: "am appointment request"
+            def rawAppointment = new Appointment(startTime, endTime, 1,1)
 
         expect: "appointment creation succeeds when valid"
             mvc.perform(MockMvcRequestBuilders
@@ -77,10 +60,18 @@ class AppointmentsControllerSpec extends Specification {
                     .accept(MediaType.APPLICATION_JSON)
                     .content(asJsonString(rawAppointment))
                     .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-
+                    .andExpect(MockMvcResultMatchers.status().is(expectedResponseCode))
+        where:
+            now                                                 | startTime            | endTime                   | expectedResponseCode
+            LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES) | now.minusMinutes(30) | startTime.plusMinutes(30) | 400 //start time can't be in past
+            LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES) | now.plusMinutes(30)  | now.minusMinutes(30)      | 400 // end time can't be before start or in past
+            LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES) | now.plusMinutes(30)  | startTime.plusMinutes(30) | 200 // 30 min appointment in future is valid
+            LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES) | now.plusMinutes(30)  | startTime.plusMinutes(10) | 400 // <30 min appt is invalid
+            // Note: we don't test conflicting appointments because that is done at the data later and this is just testing the controller
 
     }
+
+    // Note: we don't test get b/c it goes straight to the data layer
 
     String asJsonString(final Appointment appointment) {
         try {
@@ -89,9 +80,5 @@ class AppointmentsControllerSpec extends Specification {
             throw new RuntimeException(e)
         }
     }
-
-    def "GetAppointment"() {
-    }
-
 
 }
